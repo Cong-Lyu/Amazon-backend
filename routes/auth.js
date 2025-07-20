2b98e72ff22320b14765742746efaa89abe954b5
Cong Lyu <15602214230@163.com>
2025-07-20 14:24:50 +0930

    Move the routes to their own JS files.

---
+++ routes/auth.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/isvalidusertoken/:userToken', async (req, res) => {
+  const params = req.params['userToken']
+  const tokenQuery = 'SELECT *, CURRENT_TIMESTAMP AS timeNow FROM token WHERE token = ?'
+  const tokenQueryResult = await pool.query(tokenQuery, [params])
+  if(tokenQueryResult[0].length === 0) {
+    res.status(404).send('false')
+  }
+  else {
+    const tokenCreatedTime = new Date(tokenQueryResult[0][0]['created']) 
+    const timeNow = new Date(tokenQueryResult[0][0]['timeNow']) 
+    const diffHour = (timeNow - tokenCreatedTime) / (1000 * 60 * 60)
+  
+    if(diffHour > 2) {
+      res.status(200).send('false')
+    }
+    else {
+      res.status(200).send('true')
+    }
+  }
+})
+
+module.exports = router
---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+
+router.post('/login', async (req, res) => {
+  const queryUserInfo = 'SELECT * FROM users WHERE userEmail = ? AND userPassword = ?'
+  const userInfoQueryResult = await pool.query(queryUserInfo, [req.body.login, req.body.password])
+  console.log(userInfoQueryResult);
+  if(userInfoQueryResult[0].length === 0) {
+    res.status(404).send('false')
+  }
+  else {
+    //------below insert a new token for the user----//
+    const token = crypto.randomBytes(32).toString('hex')
+    const userId = userInfoQueryResult[0][0]['objectId']
+    const tokenQuery = 'INSERT INTO token(token, userId) VALUES(?, ?)'
+    await pool.query(tokenQuery, [token, userId])
+    //------below update the lastLogin column for the user----//
+    const userInfoUpdateQuery = 'UPDATE users SET lastLogin = CURRENT_TIMESTAMP() WHERE objectId = ?'
+    await pool.query(userInfoUpdateQuery, [userId])
+
+    res.status(200).json({'objectId': userId, 'user-token': token})
+  }
+})
+
+module.exports = router
