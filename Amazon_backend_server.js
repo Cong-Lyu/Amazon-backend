9f8edccaf69aebefb2d16fb974460f050fb684d3
Cong Lyu <15602214230@163.com>
2025-07-24 15:59:21 +0930

    Move cart routes to user router.

--- Amazon_backend_server.js
+++ Amazon_backend_server.js
@@ -20,45 +20,8 @@ amazonBackEndServer.use(cors());
 amazonBackEndServer.use('/api', usersRouter)
 amazonBackEndServer.use('/api/token', tokenRouter)
 
-
-
-amazonBackEndServer.get('/api/cart', async (req, res) => {
-  if(tokenAuth(req.headers['userToken'])) {
-    const allowedFields = ['userObjectId', 'productObjectId']; // only fields in this are allowed.
-    let retrievalQuery = 'SELECT * FROM cart WHERE 1=1'
-    const keyPairs = req.query
-    const keys = Object.keys(keyPairs)
-    const values = []
-    for (const key of keys) {
-      if(allowedFields.includes(key)) {
-        retrievalQuery += ` AND ${key} = ?`
-        values.push(Number(keyPairs[key]))
-      }
-      else {
-        return res.status(404).send('Wrong field name!')
-      }
-    }
-    const retrievalResult = await pool.query(retrievalQuery, values)
-    return res.status(200).json(retrievalResult[0])
-  }
-  else {
-    return res.status(404).send('You have not logged in yet!')
-  }
-})
-
-amazonBackEndServer.put('/api/cart/:id', async (req, res) => {
-  if(!req.headers['user-token']) {
-    res.status(404).send('user token is not provided!')
-  }
-  //-----we assume that the validation of the token is executed at the front end site------//
-  else {
-    const num = req.params['id']
-    const productObjectId = req.body['productObjectId']
-    const productQuantity = req.body['productQuantity']
-    const productInsertQuery = 'UPDATE cart SET productQuantity = ? WHERE productObjectId = ?'
-    const insertResult = await pool.query(productInsertQuery, [productQuantity, productObjectId])
-    res.status(200).send(insertResult)
-  }
+amazonBackEndServer.use((req, res) => {
+  res.status(404).send('Unexpected requests detected.')
 })
 
 amazonBackEndServer.listen(5000, () => {
